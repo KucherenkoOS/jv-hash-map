14771e7df8d5fadb24ee6a51b33b7cbac5f780f8
Olexander Kucherenko <amarokthegrey@gmail.com>
2025-10-07 15:46:00 +0300

    added getters and setters for Node

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -11,15 +11,39 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private int threshold;
 
     private static class Node<K, V> {
-        final K key;
-        V value;
-        final int hash;
-        Node<K, V> next;
+        private final K key;
+        private V value;
+        private final int hash;
+        private Node<K, V> next;
 
         Node(K key, V value, int hash, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.hash = hash;
+            this.next = next;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public int getHash() {
+            return hash;
+        }
+
+        public Node<K, V> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<K, V> next) {
             this.next = next;
         }
     }
